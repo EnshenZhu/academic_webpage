f0a9c743e864cb22c5f61ac9dd1073f98cd91a05
Enshen Zhu <winniecmz.zhu@gmail.com>
2024-06-28 23:33:58 -0400

    1) optimize the imported library; 2) minor upgrade of the UI

--- academic-portfolio/src/components/Navigation.js
+++ academic-portfolio/src/components/Navigation.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import { Navbar, Nav,  Container } from 'react-bootstrap';
 
 // const Navigation = () => {
 //     return (
